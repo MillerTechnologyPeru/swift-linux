c2479665c7e66e32e460420195ec1889f3c2aba2
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-26 08:34:12 -0400

    Add XWayland support to swaywm configuration for running X11 apps

--- external/package/swaywm/swaywm.mk
+++ external/package/swaywm/swaywm.mk
@@ -40,4 +40,13 @@ else
 SWAYWM_CONF_OPTS += -Dswaybar=false
 endif
 
+# XWayland: sway builds X11 support when wlroots provides it and the xcb
+# libraries are available (used to run X11 apps such as Steam under sway).
+ifeq ($(BR2_PACKAGE_WLROOTS_XWAYLAND),y)
+SWAYWM_CONF_OPTS += -Dxwayland=enabled
+SWAYWM_DEPENDENCIES += xwayland libxcb xcb-util-wm
+else
+SWAYWM_CONF_OPTS += -Dxwayland=disabled
+endif
+
 $(eval $(meson-package))
