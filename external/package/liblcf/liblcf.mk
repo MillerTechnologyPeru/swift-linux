67bb2c7e603e9032735b5e591396e094a9189bde
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-29 08:42:54 -0400

    Add a liblcf package

---
+++ external/package/liblcf/liblcf.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# liblcf
+#
+# Library only, so it installs to staging for the easyrpg core to link
+# against. Pinned to the release matching the player.
+#
+################################################################################
+
+LIBLCF_VERSION = 0.8.1
+LIBLCF_SITE = $(call github,EasyRPG,liblcf,$(LIBLCF_VERSION))
+LIBLCF_LICENSE = MIT
+LIBLCF_LICENSE_FILES = COPYING
+LIBLCF_INSTALL_STAGING = YES
+LIBLCF_DEPENDENCIES = expat icu
+
+LIBLCF_CONF_OPTS = \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DLIBLCF_ENABLE_TOOLS=OFF \
+	-DLIBLCF_ENABLE_TESTS=OFF \
+	-DLIBLCF_WITH_XML=ON \
+	-DLIBLCF_WITH_ICU=ON
+
+$(eval $(cmake-package))
