7fcf2147655cd0ba2b3655959a3005b78f607a90
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-29 08:42:54 -0400

    Add a love package

---
+++ external/package/love/love.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# love
+#
+# The linux-src release tarball ships a generated configure, so this is a
+# plain autotools build. LuaJIT is the supported interpreter (plain Lua
+# loses the JIT and some games outright require it).
+#
+################################################################################
+
+LOVE_VERSION = 11.5
+LOVE_SITE = https://github.com/love2d/love/releases/download/$(LOVE_VERSION)
+LOVE_SOURCE = love-$(LOVE_VERSION)-linux-src.tar.gz
+LOVE_LICENSE = Zlib
+LOVE_LICENSE_FILES = license.txt
+LOVE_DEPENDENCIES = luajit sdl2 openal freetype libmodplug mpg123 \
+	libvorbis libogg libtheora zlib
+
+LOVE_CONF_OPTS = --with-lua=luajit
+
+$(eval $(autotools-package))
